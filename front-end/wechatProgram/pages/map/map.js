3fe07ae796248c6ec2cdd92479e0f150b8165932
Luo Ziniu <43477985+luozn15@users.noreply.github.com>
2018-12-19 01:17:51 +0800

    完成从后端读取locations

--- front-end/wechatProgram/pages/map/map.js
+++ front-end/wechatProgram/pages/map/map.js
@@ -1,6 +1,9 @@
 // pages/map/map.js
 /*var the_url: 'http://wangtong15.com:20000/map/getlocations'*/
 var the_url ='http://127.0.0.1:8000/map'
+
+
+
 Page({
   data: {
     latitude: 39.958119466670300,
@@ -8,37 +11,84 @@ Page({
     scale: 16,
     show_location: true,
     my_modal_hidden: true,
+    //markers:[],
     markers: [{
-      iconPath: "/figs/Location (24x24).png",
-      id: 0,
+      iconPath: "/figs/location.png",
+      id: 3,
       latitude: 39.95248757392302,
       longitude: 116.28737784118653,
       width: 25,
-      height: 35
+      height: 90,
+      description: "id=0",
+      item_linked: "item0",
+      picaddr: "/photos/1ed73edb52d890c4b589d56c5149c28f.jpg",
     },
     {
-      iconPath: "/figs/Location (24x24).png",
+      iconPath: "/figs/location.png",
       id: 1,
       latitude: 39.959724734463265,
       longitude: 116.30231238098145,
       width: 25,
-      height: 35
+      height: 90,
+      description: "id=1",
+      item_linked: "item1",
+      picaddr: "/photos/b137801beac4b407567927509bce4130.jpg",
     },
     {
-      iconPath: "/figs/Location (24x24).png",
+      iconPath: "/figs/location.png",
       id: 2,
       latitude: 39.949526696601716,
       longitude: 116.30325651855469,
       width: 25,
-      height: 35
+      height: 90,
+      description: "id=2",
+      item_linked: "item2",
+      picaddr: "/photos/f756c2ec0f62d341fe128038bccde5e0.jpg",
     }],
     explore_photo_path: "/photos/1ed73edb52d890c4b589d56c5149c28f.jpg",
     title:"清华大学",
-    harvest:"水晶",
+    item_linked:"水晶",
     exp:10,
     description:"清华大学（英语：Tsinghua University，缩写作 THU），简称清华，旧称清华学堂、清华学校、国立清华大学，是一所位于中华人民共和国北京市海淀区清华园的公立大学。始建于1911年，因北京西北郊清华园而得名。"
   },
 
+  /*从服务器获取地点数据*/
+  getpositons:function () {
+    var that = this
+    var markers = []
+    wx.request({
+      url: the_url + '/getpositions', // 仅为示例，并非真实的接口地址
+      data: {
+        latitude: this.data.latitude,
+        longitude: this.data.longitude
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      method: "POST",
+      success(res) {
+        console.log(res)
+        var length=res.data.length
+        for(var i=0; i<length;  i++)
+        {
+          markers[i] = {
+            iconPath: "/figs/location.png",
+            id: i,
+            latitude: res.data["lat" + i],
+            longitude: res.data["lon" + i],
+            description: res.data["description" + i],
+            //item_linked: res.data["" + i],
+            picaddr: res.data["picaddr" + i],
+            width: 25,
+            height: 90
+            }
+        }
+        that.setData({markers:markers})
+        console.log(res)
+      }
+    })
+
+  },
 
   /*onLoad */
   onLoad: function (options) {
@@ -57,25 +107,12 @@ Page({
       }
     })
     
-    wx.request({
-      url: the_url+'/getpositions', // 仅为示例，并非真实的接口地址
-      data: {
-        latitude: this.data.latitude,
-        longitude:this.data.longitude
-      },
-      header: {
-        'content-type': 'application/x-www-form-urlencoded' // 默认值
-      },
-      method: "POST",
-      success(res) {
-        console.log(res.data)
-      }
-    })  
+    this.getpositons();
     
   },
 
-  /*从服务器获取地点数据*/
-  get_markers_data: function(){},
+
+  
   
   /*onReady */
   onReady: function (e) {
@@ -110,11 +147,15 @@ Page({
   /*点击marker点显示该点信息 */
   markertap(e) {
     var that=this
-    var marker_id=e.markerId
-    console.log(marker_id)
-    this.move_to_marker(marker_id)
+    var marker_index=e.markerId
+    //console.log(e)
+    //console.log(marker_index)
+    this.move_to_marker(marker_index)
+    //console.log(this.data.markers[marker_index])
+    
+    this.setData({ description: this.data.markers[marker_index]['description'] })
     setTimeout(function () {
-      that.show_markerinfo_page(marker_id)
+      that.show_markerinfo_page(marker_index)
     }, 500)
   },
 
@@ -126,28 +167,31 @@ Page({
   },
 
   /*打卡 */
-  to_check(){
+  to_check() {
+    var that =this
     console.log("打卡")
     //找到最近marker点，判断距离小于10米则
-    this.mapCtx.getCenterLocation({
+    /*this.mapCtx.getCenterLocation({
       type: 'gcj02',
       success: (res) => {
         let longitude = res.longitude;
         let latitude = res.latitude;
-        console.log(longitude);
-        console.log(latitude);
       }
-    })
+    })*/
+    //this.getpositons();
+    
+    
   },
 
   /*探索 */
   to_explore() {
     console.log('探索')
-    var that=this
-    var marker_id=Math.floor(Math.random() * this.data.markers.length)
-    this.move_to_marker(marker_id)
+    var that =this
+    var marker_index=Math.floor(Math.random() * this.data.markers.length)
+    this.move_to_marker(marker_index)
+    this.setData({ description: this.data.markers[marker_index]['description']})
     setTimeout(function () {
-      that.show_markerinfo_page(marker_id)
+      that.show_markerinfo_page(marker_index)
     }, 500)
   },
 

3c1ef8be6642c970a9d0ff0472ade35b39458c2a
HaipengHan15 <HaipengHan15@users.noreply.github.com>
2018-12-31 00:57:47 +0800

    character#1

--- front-end/wechatProgram/pages/character/character.js
+++ front-end/wechatProgram/pages/character/character.js
@@ -1,5 +1,5 @@
 // pages/user/user.js
-const app = getApp()
+var app = getApp()
 var the_url = 'http://wangtong15.com:20001/pet'
 /*var the_url ='http://127.0.0.1:8000/map'*/
 Page({
@@ -7,57 +7,56 @@ Page({
   /**
    * 页面的初始数据
    */
-  onReady: function (e) {
+  onReady: function(e) {
     // 使用 wx.createAudioContext 获取 audio 上下文 context
-   // this.audioCtx = wx.createAudioContext('myAudio')
+    // this.audioCtx = wx.createAudioContext('myAudio')
 
   },
 
   data: {
-    character:{
-    characterId: "",
-    characterName:"Zhangsan",
-    characterHP :"1000",
-    characterAD:"150",
-    characterDF :"80",
-    characterSP:"70",
-    characterMiss:"50%",
-    characterAppearance:"1",
-    characterExp:"100",
+    character: {
+      characterId: "",
+      characterName: "Zhangsan",
+      characterHP: "1000",
+      characterAD: "150",
+      characterDF: "80",
+      characterSP: "70",
+      characterMiss: "50%",
+      characterAppearance: "1",
+      characterExp: 0,
     },
-
-
     characterLevel: "0",
-    levelLeft:"0",
-    levelSet: ["20", "40", "80", "160", "320", "480", "640", "1000", "4000"],//等级相关
+    level: 0, // 宠物的经验
+    levelLeft: 0, // 在这级宠物已有的经验占这级总经验的百分比
+    levelSet: [0, 20, 60, 140, 300, 620, 1260, 2260], //等级相关
+    levels: 7, // 目前levelSet中最大经验值对应的级数
     characterRight: "100",
-    characterBottom: "100",//运动相关
-    audioSrc: '/audio/characterAudio.mp3',
-    characterSrc:'/photos/Tom.jpg'//调用显示声音相关
+    characterBottom: "100", //运动相关
+    audioSrc: '../../audio/characterAudio.mp3',
+    characterSrc: '../../photos/Tom.jpg' //调用显示声音相关
   },
 
   /*onLoad */
-  onLoad: function (options) {
-    var that = this;
+  onLoad: function(options) {
+    var that = this
+    var i = 1
     wx.showLoading({
-      title: '加载中',
-    }),
-    this.getCharacter();
-    var i=1;
-    while(character.characterExp>levelSet[i-1]){
-      i++;
-    }
-    var left=100*(levelSet[i]-characterExp)/levelSet[i];
-    this.setData({
-      characterLevel:i.toString,
-      levelLeft:left.toString,
-    }),
-
+        title: '加载中',
+      }),
+      that.getCharacter()
+    that.level(that.data.character['characterExp'])
+    console.log(that.data.level)
+    console.log(that.data.levelLeft)
+    // var left = 100 * (levelSet[i] - characterExp) / levelSet[i];
+    // that.setData({
+    //   characterLevel: i.toString,
+    //   levelLeft: left.toString,
+    // }),
     wx.hideLoading();
   },
 
   /*onReady */
-  onReady: function (e) {
+  onReady: function(e) {
     //wx.getFileSystemManager()
   },
 
@@ -65,42 +64,69 @@ Page({
   to_act() {
     console.log('角色触摸反响')
     this.audioCtx.play()
-
-
   },
 
-  getCharacter:function(){
+  getCharacter: function() {
     var that = this
-    var characterA
+    // var characterA = {}
     wx.request({
       url: the_url + '/petinfo', // 仅为示例，并非真实的接口地址
       data: {
         wechatId: app.globalData.openid
       },
-      
       header: {
         'content-type': 'application/x-www-form-urlencoded' // 默认值
       },
       method: "POST",
       success(res) {
-
-        characterA:{
-            characterId: res.data["id" ]; 
-            characterName: res.data["name" ];
-            characterAppearance:res.data["appearance"];
-            characterExp: res.data["exp"];
-            characterHP: res.data["health"];
-            characterAD: res.data["attack"];
-            characterDF: res.data["defend"];
-            characterSP: res.data["speed"];
-            characterMiss: res.data["dodegRate"];
-        }
-          
-        
-        that.setData({ character: characterA })
+        that.changeCharacter(res)
       }
     })
-
+  },
+  changeCharacter: function(res) {
+    var characterA = {}
+    characterA = {
+      characterId: res.data["id"],
+      characterName: res.data["name"],
+      characterHP: parseInt(res.data["health"]),
+      characterAD: parseInt(res.data["attack"]),
+      characterDF: parseInt(res.data["defend"]),
+      characterSP: parseInt(res.data["speed"]),
+      characterMiss: parseInt(res.data["dodgeRate"]) / 100.0,
+      characterAppearance: parseInt(res.data["appearance"]),
+      characterExp: parseInt(res.data["exp"]),
+    }
+    this.setData({
+      character: characterA
+    });
+  },
+  level: function(res) {
+    var left = 0
+    var leftLevels = 0
+    if (res < this.data.levelSet[this.data.levels]) {
+      for (var i = 0; i < this.data.levels; i++) {
+        if (this.data.levelSet[i] <= res && res < this.data.levelSet[i + 1]) {
+          this.setData({
+            level: i
+          })
+          this.setData({
+            levelLeft: 100*(res - this.data.levelSet[i]) / (this.data.levelSet[i + 1] - this.data.levelSet[i])
+          })
+          break
+        }
+      }
+    }
+    else{
+      left = res - this.data.levelSet[this.data.levels]
+      leftLevels = parseInt(left / (this.data.levelSet[this.data.levels] - this.data.levelSet[this.data.levels - 1]))
+      left = res - leftLevels * (this.data.levelSet[this.data.levels] - this.data.levelSet[this.data.levels - 1])
+      this.setData({
+        level: this.data.levels + leftLevels
+      })
+      this.setData({
+        levelLeft: 100*left / (this.data.levelSet[levels] - this.data.levelSet[levels-1])
+      })
+    }
   }
 
 })
--- front-end/wechatProgram/pages/community/community.js
+++ front-end/wechatProgram/pages/community/community.js
@@ -10,42 +10,49 @@ Page({
   },
 
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     //加载数据
-    wx.request({
-      url: the_url + '/nearbyinfo',
-      data: {
-        user: app.globalData.openid,
-        latitude: app.globalData.latitude,
-        longitude: app.globalData.longitude
-      },
-      header: {
-        'content-type': 'application/x-www-form-urlencoded' // 默认值
-      },
-      method: 'POST',
-      success: function (res) {
-        that.order(res);
-      },
-    });
+    wx.showLoading({
+        title: '加载中',
+      }),
+      wx.request({
+        url: the_url + '/nearbyinfo',
+        data: {
+          user: app.globalData.openid,
+          latitude: app.globalData.latitude,
+          longitude: app.globalData.longitude
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded' // 默认值
+        },
+        method: 'POST',
+        success: function(res) {
+          that.order(res);
+        },
+      }),
+      wx.hideLoading();
   },
-  order: function(e){
+  order: function(e) {
     var list2 = [];
     if (e.data.success == 0) { // 不存在这个用户
-    }
-    else {
-      this.setData({ list: e.data });
-      
+    } else {
+      this.setData({
+        list: e.data
+      });
+
       for (var i = 0; i < e.data.length; i++) {
         list2[i] = {
           avatarUrl: e.data["avatarUrl" + i],
           exp: e.data["exp" + i],
-          isLiked: e.data["isLiked" + i] == 0? false:true,
+          isLiked: e.data["isLiked" + i] == 0 ? false : true,
           nickname: e.data["nickname" + i],
-          wechatId: e.data["wechatId" + i],  // 就是openid
+          wechatId: e.data["wechatId" + i], // 就是openid
         }
       };
-      this.setData({ list2: list2 })
+      this.setData({
+        list2: list2
+      })
     }
   },
   // returntape: function(e){
@@ -57,11 +64,11 @@ Page({
   // tape2: function (e) {
   //   this.setData({ hidden: false })
   // },
-  zan: function (e) {
+  zan: function(e) {
     var _index = e.currentTarget.dataset.index;
     var _list2 = [...this.data.list2]; // list2的引用
     _list2[_index].isLiked = !this.data.list2[_index].isLiked;
-    
+
     this.setData({
       list2: _list2
     })
@@ -69,15 +76,14 @@ Page({
       url: the_url + '/like',
       data: {
         user: app.globalData.openid, // 点赞/取消点赞的人的openid
-        friend: this.data.list2[_index].wechatId,  // 被点赞/被取消点赞的人的openid
-        type: this.data.list2[_index].isLiked? 1:0  // true: 点赞; false: 取消点赞
+        friend: this.data.list2[_index].wechatId, // 被点赞/被取消点赞的人的openid
+        type: this.data.list2[_index].isLiked ? 1 : 0 // true: 点赞; false: 取消点赞
       },
       header: {
         'content-type': 'application/x-www-form-urlencoded' // 默认值
       },
       method: 'POST',
-      success: function (res) {
-      },
+      success: function(res) {},
     });
   }
 })

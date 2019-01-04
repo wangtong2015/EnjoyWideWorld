39f20f4ce0ef4cb77336c0c93c7374ff5b89d4cc
HaipengHan15 <HaipengHan15@users.noreply.github.com>
2019-01-04 13:41:18 +0800

    community备选方案

--- front-end/wechatProgram/pages/community/community.js
+++ front-end/wechatProgram/pages/community/community.js
@@ -105,28 +105,85 @@ Page({
         attacker = res1
         that.getPetInfo(that.data.list2[_index].wechatId).then((res2) => {
           attacked = res2
-          var str1 = JSON.stringify(attacker)
-          var str2 = JSON.stringify(attacked)
-          wx.navigateTo({
-            url: '/pages/fight/fight?str1=' + str1 + '&str2=' + str2,
-            success: function (res) {
-              // success
-            },
-            fail: function (res) {
-              // fail
-            },
-            complete: function (res) {
-              // complete
-            }
-          });
+          if (Math.random() < 0.5) {
+            wx.showToast({
+              title: '挑战成功',
+              duration: 2000,
+              icon: 'none',
+              success: function(){
+                console.log()
+                wx.request({
+                  url: the_url + '/afterbattle',
+                  data: {
+                    attacker: res1.userId,
+                    defender: res2.userId,
+                    petId: res1.id,
+                    addExp: res1.exp > res2.exp ? 50 : 100
+                  },
+                  success: (res) => { },
+                  fail: () => { },
+                  complete: () => {
+                  }
+                })
+              }
+            })
+            
+          }else{
+            wx.showToast({
+              title: '挑战失败',
+              duration: 2000,
+              icon: 'none',
+              success: function () {
+                wx.request({
+                  url: the_url + '/afterbattle',
+                  data: {
+                    attacker: res1.userId,
+                    defender: res2.userId,
+                    petId: res1.id,
+                    addExp: 25
+                  },
+                  success: (res) => { },
+                  fail: () => { },
+                  complete: () => {
+                  }
+                })
+              }
+            })
+          };
+          // var str1 = JSON.stringify(attacker)
+          // var str2 = JSON.stringify(attacked)
+          // wx.navigateTo({
+          //   url: '/pages/fight/fight?str1=' + str1 + '&str2=' + str2,
+          //   success: function (res) {
+          //     // success
+          //   },
+          //   fail: function (res) {
+          //     // fail
+          //   },
+          //   complete: function (res) {
+          //     // complete
+          //   }
+          // });
         }).catch((res2) => {
           console.log(res2)
         })
       }).catch((res1) => {
         console.log(res1)
       })
-      console.log(attacked)
-
+    }
+    if (app.globalData.openid == that.data.list2[_index].wechatId){
+      wx.showToast({
+        title: '不能挑战自己',
+        duration: 2000,
+        icon: 'none'
+      })
+    }
+    if (_list2[_index].isFighted){
+      wx.showToast({
+        title: '已挑战过此人',
+        duration: 2000,
+        icon: 'none'
+      })
     }
   },
   getPetInfo: function(e) {
